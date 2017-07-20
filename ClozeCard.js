e132fe465b88f5ceb5219b75546358b59922e564
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-07-19 23:56:33 -0700

    ClozeCard constructor

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,7 +1,20 @@
-function ClozeCard(text, cloze) {
+var BasicCards = require('./BasicCard.js');
 
-	this.text = text;
+function ClozeCard(fullText, cloze) {
+	this.fullText = fullText;
 	this.cloze = cloze;
+};
+
+ClozeCard.prototype.getFullText = function(fullText) {
+	return this.fullText;
+}
+
+ClozeCard.prototype.getPartialText = function(fullText, cloze) {
+	return this.fullText.replace(this.cloze, '...');
+}
+
+ClozeCard.prototype.brokenCloze = function(fullText, cloze) {
+	throw new Error('The cloze deletion did not appear.');
 }
 
 module.exports = ClozeCard;
