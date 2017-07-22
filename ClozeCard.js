141d1f3824f3b204794b84a8667f0a343915edc9
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-07-21 22:57:58 -0700

    saving work

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,18 +1,48 @@
-function ClozeCard(fullText, cloze) {
-	this.fullText = fullText;
-	this.cloze = cloze;
-};
+var inquirer = require('inquirer');
+var fs = require('fs');
 
-ClozeCard.prototype.getFullText = function(fullText) {
-	return this.fullText;
+var cardData = require('./cloze.json');
+
+function ClozeCard(fullText,answer) {
+	var clozePositions = clozeDelete(fullText,answer);
+
+	this.partial = getPartial(fullText,clozePositions);
+  	
+  	this.answer = answer; 
+
+	function clozeDelete(fullText,answer){
+		var start = fullText.indexOf(answer);
+		if(start !== -1){
+			return [start, start+answer.length];
+		}
+		throw new Error("Could not find answer in fullText");
+	}
+		function getPartial(fullText,clozePositions){
+			var start = fullText.slice(0,clozePositions[0]);
+			var end = fullText.slice(clozePositions[1],fullText.length);
+			return start+" ... "+end;
+		}
 }
 
-ClozeCard.prototype.getPartialText = function(fullText, cloze) {
-	return this.fullText.replace(this.cloze, '...');
+function createNewCard(){
+	inquirer.prompt([{
+		type:"input",
+		name:"fullText",
+		message:"What is the full text of the flashcard you want to make?"
+	},{
+		type:"input",
+		name:"answer",
+		message:"What is the answer to the flashcard?"
+	}]).then(function(inputs){
+		var card = new ClozeCard(inputs.fullText,inputs.answer);
+		console.log(card);
+		cardData.push(card); 
+	var newCardData = JSON.stringify(cardData,null,'\t');
+	fs.writeFile('./cloze.json',newCardData,function(err){
+		if(err)throw err;
+		console.log("Done");
+	})
+  })
 }
 
-ClozeCard.prototype.brokenCloze = function(fullText, cloze) {
-	throw new Error('The cloze deletion did not appear.');
-}
-
-module.exports = ClozeCard;
+createNewCard();
